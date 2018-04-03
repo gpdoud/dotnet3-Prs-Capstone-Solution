8dd157f42eccc4b3a0a5b7821324bb0b3f18ed27
Greg doud <gpdoud@gmail.com>
2018-04-03 08:40:48 -0400

    Created AllowCrossSiteJsonAttribute to address the CORS issue

--- PrsCapstoneProject/Utility/AllowCrossSiteJsonAttribute.cs
+++ PrsCapstoneProject/Utility/AllowCrossSiteJsonAttribute.cs
@@ -5,7 +5,18 @@ namespace PrsCapstoneProject.Utility {
 
 	public class AllowCrossSiteJsonAttribute : ActionFilterAttribute {
 		public override void OnActionExecuting(ActionExecutingContext filterContext) {
-			filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
+			//filterContext.RequestContext.HttpContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
+			//base.OnActionExecuting(filterContext);
+			// We'd normally just use "*" for the allow-origin header, 
+			// but Chrome (and perhaps others) won't allow you to use authentication if
+			// the header is set to "*".
+			// TODO: Check elsewhere to see if the origin is actually on the list of trusted domains.
+			var ctx = filterContext.RequestContext.HttpContext;
+			var origin = ctx.Request.Headers["Origin"];
+			var allowOrigin = !string.IsNullOrWhiteSpace(origin) ? origin : "*";
+			ctx.Response.AddHeader("Access-Control-Allow-Origin", allowOrigin);
+			ctx.Response.AddHeader("Access-Control-Allow-Headers", "*");
+			ctx.Response.AddHeader("Access-Control-Allow-Credentials", "true");
 			base.OnActionExecuting(filterContext);
 		}
 	}
